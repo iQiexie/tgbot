b20e4461bc1aee7f9998202a3a032b5a3b9d5561
iQiexie <email@email.com>
2025-03-20 18:46:41 +0300

    fix

--- app/game/endpoints.py
+++ app/game/endpoints.py
@@ -19,11 +19,13 @@ async def get_current_user(webapp_data: str = Depends(AuthUserHeader)) -> Webapp
 async def get_invoice_url(
     current_user: Annotated[WebappData, Depends(get_current_user)],
     data: InvoiceData,
-) -> str:
-    return await bot.create_invoice_link(
+) -> dict[str, str]:
+    resp = await bot.create_invoice_link(
         title=data.title,
         description=data.description,
         payload=str(current_user.telegram_id),
         currency="XTR",
         prices=[LabeledPrice(label=data.price_label, amount=data.price_amount)],
     )
+
+    return {"data": resp}
